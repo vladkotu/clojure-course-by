f9c303cff67cf128e01def21f5c41cf969772b94
Vladimir Vladimirovich <vladkotu@pop-os.localdomain>
2020-04-27 14:04:42 +0300

    simplify client and server to single query per socket

--- task02-client/src/task02/task02_client.clj
+++ task02-client/src/task02/task02_client.clj
@@ -1,58 +1,19 @@
 (ns task02.task02-client
-  (:refer-clojure :exclude [read-line])
   (:require [clojure.java.io :as io])
   (:import [java.net Socket InetAddress InetSocketAddress SocketTimeoutException])
   (:gen-class))
 
-(defn create-socket [hostname port]
-  (Socket. hostname port))
-
-(defn close-socket [socket]
-  (.close socket))
-
-(defn write [socket message]
-  (doto (io/writer socket)
-    (.write (str message \newline))
-    (.flush)))
-
-(def get-reader
-  (memoize (fn [socket] (io/reader socket))))
-
-(defn read-line [socket]
-  (doto (get-reader socket)
-    (.readLine)))
-
-(defn read-line [socket]
-  (with-open [rdr (io/reader socket)]
-    (.readLine rdr)))
-
-(def socket1 (create-socket "127.0.0.1" 9999))
-(def socket2 (create-socket "127.0.0.1" 9999))
+(defn query [command]
+  (let [sock (Socket. "127.0.0.1" 9999)]
+    (with-open [rdr (io/reader sock)
+                wrt (io/writer sock)]
+      (let [result (delay (.readLine rdr))]
+        (doto wrt
+          (.write command)
+          (.write (str \newline))
+          (.flush))
+        (when-let [data @result]
+          (clojure.edn/read-string data))))))
 
 
-;; (dotimes [n 100]
-;;   (write socket1 "hola")
-;;   (write socket2 "espanolo")
-;;   )
-
-
-(doseq [[name socket] [["socket1" socket1]
-                       ["socket2" socket2]]]
-  (future
-    (with-open [rdr (io/reader socket)]
-      (loop [line (.readLine rdr)]
-        (println "in shutdown" (.isInputShutdown socket))
-        (println "isClosed" (.isClosed socket))
-        (if (= "quit" line)
-          (do (println (str name " socket closed"))
-              (close-socket socket))
-          (do
-            (println (str name " socket recieved: " line))
-            (Thread/sleep (rand-int 1000))
-            (recur (.readLine rdr))))))))
-
-(close-socket socket1)
-(close-socket socket2)
-
-(.isClosed socket1)
-(.isClosed socket2)
+(query "select student where surname = Ivanov")
--- task02/src/task02/network.clj
+++ task02/src/task02/network.clj
@@ -1,72 +1,58 @@
 (ns task02.network
-  (:use [task02 helpers query])
+  ;; (:use [task02 helpers query])
   (:require [clojure.java.io :as io]
-            [clojure.string :as str])
+            [clojure.string :as str]
+            [clojure.edn :as edn]
+            [task02.query :as q]
+            [task02.db :as db])
   (:import [java.net Socket ServerSocket InetAddress InetSocketAddress SocketTimeoutException]))
 
 ;; Объявить переменную для синхронизации между потоками. Воспользуйтесь promise
 (def ^:private should-be-finished (promise))
 
-;; Hint: *in*, *out*, io/writer, io/reader, socket.getOutputStream(), socket.getInputStream(), socket.close(), binding
-;;       deliver, prn
+(set! *warn-on-reflection* true)
+
+(pr-str [{:a 1}])
+
 (defn handle-request [^Socket sock]
-  (:implement-me ;; переопределить *in* & *out* чтобы они указывали на входной и выходной потоки сокета
-   (try
-     (let [s (read-line)] ;; считать данные из переопределенного *in*
-       (if (= (str/lower-case s) "quit")
-         (deliver should-be-finished true) ;;; 1) сообщить основному потоку что мы завершаем выполнение.
-         :implement-me ;;; 2) выполнить запрос при помощи perform-query и записать
-               ;;; результат в переопределенный *out*
-         ))
-     (catch Throwable ex
-       (println "Exception: " ex))
-     (finally
-       (.close sock))))) ;;; закрыть сокет
+  (try
+    (with-open [rdr (io/reader sock)
+                wrt (io/writer sock)]
+      (let [command (.readLine ^java.io.BufferedReader  rdr)]
+        (doto wrt
+          (.write (pr-str (q/perform-query command)))
+          (.write (str \newline))
+          (.flush))))
+    (catch Throwable ex
+      (println "Exception: " ex))
+    (finally
+      (.close sock))))
 
 
 ;; Hint: future, deliver
-(defn- run-loop [server-sock]
+(defn- run-loop [^ServerSocket server-sock]
   (try
-    (println "run-loop")
-    (let [^Socket sock (.accept server-sock)]
+    (let [sock (.accept server-sock)]
       (future
-        (println "new thread")
-        (with-open [rdr (io/reader sock)
-                    wr  (io/writer sock)]
-          (loop [line (.readLine rdr) comms 3]
-            (if (zero? comms)
-              (do (println "goint out:")
-                  (doto wr
-                    (.write (str "quit" \newline))
-                    (.flush)))
-              (do
-                (println (str "recieved: " line))
-                (doto wr
-                  (.write (str "processed: " line \newline))
-                  (.flush))
-                (recur (.readLine rdr) (dec comms))))))
-        (println "close socket")
-        (.close sock)))
+        (handle-request sock)))
     (catch SocketTimeoutException ex)
     (catch Throwable ex
       (println "Got exception" ex)
-      (deliver should-be-finished true) ;; сообщить основному потоку что мы завершаем выполнение
-          ;; для этого необходимо установить переменную should-be-finished в true
-      )))
+      (deliver should-be-finished true))))
 
-(defn run [port]
-  (let [sock-addr (InetSocketAddress. "127.0.0.1" port)
+(defn run [^long port]
+  (let [sock-addr     (InetSocketAddress. "127.0.0.1" port)
         server-socket (doto (ServerSocket.)
                         (.setReuseAddress true)
                         (.setSoTimeout 1000)
                         (.bind sock-addr))]
     (loop [_ (run-loop server-socket)]
-      (when-not (realized? should-be-finished) ;; следующий запрос если работа не завершается...
+      (when-not (realized? should-be-finished)
         (recur (run-loop server-socket))))
-    (println "close server")
     (.close server-socket)))
 
-(run 9999)
+;; (db/load-initial-data)
+;; (run 9999)
 
-(def ^:private should-be-finished (promise))
-(realized? should-be-finished)
+;; (def ^:private should-be-finished (promise))
+;; (realized? should-be-finished)
