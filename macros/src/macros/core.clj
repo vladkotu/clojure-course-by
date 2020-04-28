11d91b94449cf3dba480396a9cd2d0ce336b7d35
vladkotu <volodymyr.kotushenko@freshcodeit.net>
2020-04-28 09:25:18 +0300

    simplify if-all-let macro

--- macros/src/macros/core.clj
+++ macros/src/macros/core.clj
@@ -24,29 +24,15 @@ x1 ;; => 10
 ;;;;;;;;;;;;;;;;;;; if all let macro ;;;;;;;;;;;;;;;;;
 (alter-var-root #'clojure.pprint/*print-suppress-namespaces* (constantly true))
 
-(defmacro more-than-let [bindings & body]
-  (if (= (count bindings) 0)
-    `(do ~@body)
-    `(let ~(subvec bindings 0 2)
-       (more-than-let ~(subvec bindings 2) ~@body))))
-
-(with-out-str
-  (clojure.pprint/pprint
-   (clojure.walk/macroexpand-all
-    '(more-than-let [zu 1
-                     eu 2
-                     xx nil]
-                    :reutrn))))
-;; => "(let* [zu 1] (let* [eu 2] (let* [xx nil] (do :reutrn)))) "
-
 (defmacro if-all-let [bindings then else]
   (let [parties   (partition 2 bindings)
         positive  (keep (fn [[_ tst]] `(when ~tst) tst) parties)
         parties'  (count parties)
         positive' (count positive)]
-    `(more-than-let ~bindings (if (= ~parties' ~positive')
-                                ~then
-                                ~else))))
+    `(let ~bindings
+      (if (= ~parties' ~positive')
+        ~then
+        ~else))))
 
 (with-out-str
   (clojure.pprint/pprint
@@ -56,10 +42,7 @@ x1 ;; => 10
                   xx nil]
                  :than
                  :else))))
-;; => (let* [zu 1]
-;;     (let* [eu 2]
-;;       (let* [xx nil]
-;;         (do (if (= 3 2) :than :else)))))
+;; => "(let* [zu 1 eu 2 xx nil] (if (= 3 2) :than :else))\n"
 
 (if-all-let [z 1
              x 2
