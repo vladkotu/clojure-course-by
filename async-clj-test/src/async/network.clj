a417799a4afdefc02718e7a142eaaeab72936ca3
vladkotu <volodymyr.kotushenko@freshcodeit.net>
2020-04-30 13:45:41 +0300

    search bing every 500 ms

--- async-clj-test/src/async/network.clj
+++ async-clj-test/src/async/network.clj
@@ -2,27 +2,49 @@
   (:require [clojure.core.async :as a :refer [<!! >!!]]
             [clojure.pprint :as pp]
             [clojure.data.json :as json]
+            [cheshire.core :as che]
             [clj-http.client :as client]))
 
 
 ;;; networking
 ;; (def surl "https://serpapi.com/search.json?q=Coffee&location=Kharkiv&hl=en&gl=ua")
 
-(def surl "https://serpapi.com/search.json")
 
-(client/get surl {
-                  :async? true
-                  ;; :as :json
-                  :query-params {:q "Coffee"
-                                 :location "Kharkiv"
-                                 :hl "en"
-                                 :gl "ua"}}
-            (fn [response]
+(def surl "https://api.cognitive.microsoft.com/bing/v7.0/search")
 
-              ;; (println response)
-              (println (json/read-str response))
-              #_(let [res (json/read-str response)]
-                  (println res)
-                  res))
-            (fn [err]
-              (println err)))
+(def locations {:kh "Kharkiv"
+                :zp "Zaporizhzhia"
+                :ky "Kyiv"})
+
+(defn search< [{:keys [q l]} c]
+  (client/get
+   surl {:async?  true
+         :headers {"Ocp-Apim-Subscription-Key" "d6735678aef84ffd9b2518f94442d6cd"}
+         :query-params
+         {:q       (str q " near " (get locations l))
+          :cc      "ua"
+          :mkt     "ua_EN"
+          :setLang "en"
+          :count   3}}
+   (fn [response]
+     (let [data (che/parse-string (:body response) true)
+           places (-> data :webPages :value)
+           result (map #(select-keys % [:name :snippet :url]) places)]
+       (a/put! c result)))
+   (fn [err]
+     (println "ERROR")
+     (clojure.pprint/pprint err)))
+  c)
+
+(def queries
+  (for [q ["Burger" "Coffee" "Fitness" "Hotel"]
+        l (keys locations)]
+    {:q q :l l}))
+
+(let [c (a/chan 100)]
+  (a/go-loop [queries queries]
+    (when (seq queries)
+      (println (a/<! (search< (first queries) c)))
+      (println "Waiting")
+      (a/<! (a/timeout 500))
+      (recur (rest queries)))))
