d05c1be550e6f44900639a86485b924f9d675838
vladkotu <volodymyr.kotushenko@freshcodeit.net>
2020-04-30 11:20:12 +0300

    ignore this for now

--- async-clj-test/src/async/async_clj_test.clj
+++ async-clj-test/src/async/async_clj_test.clj
@@ -1,5 +1,5 @@
 (ns async.async-clj-test
-  (:require [clojure.core.async :as a]
+  (:require [clojure.core.async :as a :refer [<!! >!!]]
             [clojure.pprint :as pp]))
 
 (def c1
@@ -40,7 +40,6 @@
        :suppress-namespaces (not qualificate#))
       (println))))
 
-
 (defmacro while-let [binding & body]
   (let [[form test] binding]
     `(loop [times# 1024]
@@ -66,7 +65,7 @@
 ;;         (recur (dec times__21423__auto__))))))
 
 (a/go (while-let [res (a/<! queue)]
-    (println res)))
+                 (println res)))
 
 ;; 10
 ;; 9
@@ -78,3 +77,27 @@
 ;; 3
 ;; 2
 ;; 1
+
+;; From TBaldridge -- its too hard to get for right now
+(defn to-proc< [in]
+  (let [out (a/chan 1)]
+    (a/pipe in out)
+    out))
+
+(defn pipeline< [data c]
+  (let [pip (partition 2 data)]
+    (reduce
+     (fn [prev-c [n f]]
+       (a/merge
+        (for [_ (range n)]
+          (to-proc< (a/map< f prev-c)))))
+     c
+     pip)))
+
+(let [c (a/chan 10)]
+  (>!! c 42)
+  (<!! (pipeline< [4 inc
+                   1 dec
+                   2 inc
+                   3 #(* % %)
+                   2 str] c)))
