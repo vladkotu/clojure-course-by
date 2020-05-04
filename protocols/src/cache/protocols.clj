d7834fa41b068c1668bbf5368d3af6930d2f118a
vladkotu <volodymyr.kotushenko@freshcodeit.net>
2020-05-04 19:32:41 +0300

    implement memoization with default naive strategy

--- protocols/src/cache/protocols.clj
+++ protocols/src/cache/protocols.clj
@@ -7,7 +7,9 @@
   (miss   [cache e result] "Called when no item is found"))
 
 (comment
-  ;; Object hashCoe implementation will give this:
+  ;; Primers: https://stackoverflow.com/questions/26622511/clojure-value-equality-and-sets
+  ;;          https://stackoverflow.com/questions/9229434/maps-and-records-equality-in-clojure
+  ;; Object hashCode implementation will give this:
   (into #{} [(BaseCache. {:a 1}) (BaseCache. {:a 1})])
   ;; => #{#object[cache.protocols.BaseCache 0x3d72637f "<{:a 1}>"]}
 
@@ -47,5 +49,49 @@
   (miss [_ item result]
     (BaseCache. (assoc cache item result))))
 
-(def cache (BaseCache. {:k :v}))
-(lookup (miss cache :k2 :v2) :k2)
+(comment
+  (.toString (BaseCache. {:k :v}))
+  (def cache (BaseCache. {:k :v}))
+  (lookup (BaseCache. {:k :v2}) :k))
+
+(defn hit-or-miss [cache f item]
+  (if (has? cache item)
+    (hit cache item)
+    (miss cache item (delay (apply f item)))))
+
+(do
+  (defn foo [& nums]
+    (Thread/sleep 1000)
+    (reduce + (filter number? nums)))
+  (foo 1 32 nil 'a "s")
+  (def args [1 2 3 'sym nil "a" 4])
+  (def basecache (hit-or-miss (BaseCache. {}) foo args))
+  (with-out-str (time (print (str (deref (lookup basecache args)) " => "))))
+  ;; => "10 => \"Elapsed time: 1000.388802 msecs\"\n"
+  (with-out-str (time (print (str @(lookup basecache args) " => "))))
+  ;; => "10 => \"Elapsed time: 0.187338 msecs\"\n"
+  )
+(do
+  (def args2 [9 8 7])
+  (def cache (atom (BaseCache. {})))
+  (swap! cache hit-or-miss foo args)
+  (swap! cache hit-or-miss foo args2)
+  @(lookup @cache args)
+  ;; => 10
+  @(lookup @cache args2)
+  ;; => 24
+  )
+
+(defn memo
+  ([f] (memo f (BaseCache. {})))
+  ([f strategy & seed-args]
+   {:pre [(satisfies? PCache strategy)]}
+   (let [cache-state (atom strategy)]
+     (fn [& args]
+       (let [item   (concat seed-args args)
+             result (swap! cache-state hit-or-miss f item)]
+         (deref (lookup result item)))))))
+
+(def c-sum (memo #(do (Thread/sleep 1000)
+                      (+ %1 %2))))
+(c-sum 2 2)
